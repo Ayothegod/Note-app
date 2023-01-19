20aacf7d1c960290378fa730edfc7b91d136bf97
Ayomide <heyayomideadebisi@gmail.com>
2023-01-19 14:43:57 +0100

    initial commit

---
+++ pages/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <div className='flex space-x-4xl '>
+    <p className='text-blue-dark text-3xl '>Header</p>
+    <p className='text-blue text-3xl '>Hello</p>
+    </div>
+  )
+}
+
+export default Header
